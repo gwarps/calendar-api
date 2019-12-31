2bcdcbcf42c9d39dc71b8630199f8d60eaa3217e
Puneet Singh <puneet.mir@gmail.com>
2019-12-31 20:45:54 +0530

    range api for events

--- src/main/java/com/cldr/event/EventController.java
+++ src/main/java/com/cldr/event/EventController.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.format.annotation.DateTimeFormat;
 import java.util.List;
+import java.time.ZonedDateTime;
 
 import com.cldr.repository.EventRepository;
 
@@ -20,8 +22,22 @@ public class EventController {
 
     // Get user events
     @GetMapping("/users/{user_id}/events")
-    List<Event> getUserEvents(@PathVariable("user_id") Integer userId) {
-        return eventRepository.findEventsByUser(userId);
+    List<Event> getUserEvents(
+        @PathVariable("user_id") Integer userId,
+        @RequestParam(value="start_time", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime startTime,
+        @RequestParam(value="end_time", required = false) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) ZonedDateTime endTime) {
+
+        if (startTime == null && endTime == null ) {
+            return eventRepository.findEventsByUser(userId);
+        }
+        if (startTime != null && endTime == null) {
+            return eventRepository.findEventsByUserAndGreaterThenStartDate(userId, startTime);
+        }
+        if (startTime == null && endTime != null) {
+            return eventRepository.findEventsByUserAndLessThenEndDate(userId, endTime);
+        }
+
+        return eventRepository.findEventsByUserAndDateRange(userId, startTime, endTime);
     }
 
     // Get event by id
